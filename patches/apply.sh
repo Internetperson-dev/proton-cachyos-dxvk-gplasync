be630315078c5f1de13954eb78c5d06bf7d4f4f4
Stelios Tsampas <loathingkernel@gmail.com>
2025-02-10 11:34:02 +0200

    patches: add script to apply minor patches

---
+++ patches/apply.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eu
+
+patch_cmd() {
+    echo "Applying:: $(basename "$1")"
+    patch -Np1 -i "$1"
+}
+
+here="$(dirname "$(realpath "$0")")"
+
+pushd "$here"/../gstreamer || exit 1
+    patch_cmd "$here"/gstreamer/gstreamer-5509.patch
+    patch_cmd "$here"/gstreamer/gstreamer-5511.patch
+popd || exit 1
+
+pushd "$here"/../openfst || exit 1
+    patch_cmd "$here"/openfst/openfst-879f09d2ac799cca99b78de3442194ebbe29d24a.patch
+popd || exit 1
