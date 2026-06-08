3aab5dd63159d3b09619e5dec637d6d1a0d21bcb
Katharina Dröge <kate@commandmc.de>
2026-06-08 21:44:33 +0200

    dxvk-sarek: utilities: golf some abstractions onto the Vulkan bindings
    This makes client code easier to read, which is important in case we want to use it elsewhere
    
    - Added automatic error handling for functions returning `VkResult`
    - Pythonic abstractions around all currently used functionality, including a context manager for VkInstance
    - Dropped enum name from enum members (we don't have to worry about namespace pollution here)

--- utilities.py
+++ utilities.py
@@ -39,74 +39,69 @@ log = Log()
 
 
 def primary_gpu_supports_vulkan(major: int, minor: int, patch: int = 0, /, device_filter: str = '') -> bool:
-    from ctypes import cast, POINTER, c_char_p, c_uint32, c_void_p
+    from vulkan import (VulkanError, VulkanInstance, VkPhysicalDeviceType, VulkanPhysicalDeviceAllFeatures, VulkanExtensionProperties, VulkanPhysicalDeviceProperties)
 
-    from vulkan import (vkCreateInstance, vkDestroyInstance, VkInstanceCreateInfo, VkStructureType, VkApplicationInfo,
-                        vkEnumeratePhysicalDevices, VkPhysicalDeviceProperties2, vkGetPhysicalDeviceProperties2,
-                        VK_MAKE_VERSION, VkPhysicalDeviceType)
+    discrete_gpus: list[
+        tuple[VulkanPhysicalDeviceProperties, list[VulkanExtensionProperties], VulkanPhysicalDeviceAllFeatures]] = []
+    integrated_gpus: list[
+        tuple[VulkanPhysicalDeviceProperties, list[VulkanExtensionProperties], VulkanPhysicalDeviceAllFeatures]] = []
+    virtual_gpus: list[
+        tuple[VulkanPhysicalDeviceProperties, list[VulkanExtensionProperties], VulkanPhysicalDeviceAllFeatures]] = []
 
-    vk_instance = c_void_p(0)
+    try:
+        with VulkanInstance() as instance:
+            for device in instance.enumerate_physical_devices():
+                properties, idproperties = device.get_properties()
 
-    create_instance_result = vkCreateInstance(VkInstanceCreateInfo(
-        sType=VkStructureType.VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO.value,
-        pNext=0,
-        flags=0,
-        pApplicationInfo=cast(0, POINTER(VkApplicationInfo)),
-        enabledLayerCount=0,
-        ppEnabledLayerNames=cast(0, POINTER(c_char_p)),
-        enabledExtensionCount=0,
-        ppEnabledExtensionNames=cast(0, POINTER(c_char_p))
-    ), c_void_p(0), vk_instance)
-    if create_instance_result != 0:
-        # FIXME: What do we do in a failure case?
+                if device_filter:
+                    device_uuid = ''.join(f'{n:x}' for n in idproperties.deviceUUID)
+                    if device_filter != device_uuid and device_filter not in properties.deviceName:
+                        continue
+
+                features = device.get_features()
+                extensions = device.get_extensions()
+
+                match properties.deviceType:
+                    case VkPhysicalDeviceType.DISCRETE_GPU:
+                        discrete_gpus.append((properties, extensions, features))
+                    case VkPhysicalDeviceType.INTEGRATED_GPU:
+                        integrated_gpus.append((properties, extensions, features))
+                    case VkPhysicalDeviceType.VIRTUAL_GPU:
+                        virtual_gpus.append((properties, extensions, features))
+
+            # this handles the case that the device filter is malformed and doesn't match any GPUs.
+            # Instead of a silent fallback to sarek, let it break later on with upstream dxvk
+            if not discrete_gpus and not integrated_gpus and not virtual_gpus:
+                raise VulkanError
+
+    except VulkanError:
         return True
 
-    num_devices = c_uint32(0)
-    num_devices_result = vkEnumeratePhysicalDevices(vk_instance, num_devices, cast(0, POINTER(c_void_p)))
-    if num_devices_result != 0:
-        return True
-
-    devices = (c_void_p * num_devices.value)(0)
-    devices_handle_result = vkEnumeratePhysicalDevices(vk_instance, num_devices, cast(devices, POINTER(c_void_p)))
-    if devices_handle_result != 0:
-        return True
-
-    properties2 = VkPhysicalDeviceProperties2()
-    version_to_check = VK_MAKE_VERSION(major, minor, patch)
-    # True/False -> supported/not supported, -1 -> device type not present
-    discrete_gpu_supported = -1
-    integrated_gpu_supported = -1
-    virtual_gpu_supported = -1
-    for device in devices:
-        # FIXME: Can this fail?
-        vkGetPhysicalDeviceProperties2(device, properties2)
-
-        if device_filter not in properties2.properties.deviceName.decode():
-            continue
-
-        device_type = properties2.properties.deviceType
-        supports_requested_version = properties2.properties.apiVersion >= version_to_check
-
-        if discrete_gpu_supported != True and device_type == VkPhysicalDeviceType.VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU.value:
-            discrete_gpu_supported = supports_requested_version
-        if integrated_gpu_supported != True and device_type == VkPhysicalDeviceType.VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU.value:
-            integrated_gpu_supported = supports_requested_version
-        if virtual_gpu_supported != True and device_type == VkPhysicalDeviceType.VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU.value:
-            virtual_gpu_supported = supports_requested_version
-
-    vkDestroyInstance(vk_instance, c_void_p(0))
-
-    if discrete_gpu_supported != -1:
-        return discrete_gpu_supported
-    if integrated_gpu_supported != -1:
-        return integrated_gpu_supported
-    if virtual_gpu_supported != -1:
-        return virtual_gpu_supported
-
-    return False
+    gpus_to_look_at = discrete_gpus or integrated_gpus or virtual_gpus
+    required_features = (
+        features.features12.descriptorIndexing,
+    )
+    required_extensions = (
+    )
+    return any(
+        properties.apiVersion >= (major, minor, patch) and all(required_features) and all(required_extensions)
+        for properties, extensions, features in gpus_to_look_at
+    )
 
 
 if __name__ == '__main__':
+    print("\nDifferent versions")
+    print(primary_gpu_supports_vulkan(1,2))
+    print(primary_gpu_supports_vulkan(1,3))
+    print(primary_gpu_supports_vulkan(1,4))
+    print(primary_gpu_supports_vulkan(1,5))
+
+    print("\nWith filter")
+    print(primary_gpu_supports_vulkan(1,1, device_filter="744c6095a55e1f94172f15a2c18ea17a"))
+    print(primary_gpu_supports_vulkan(1,1, device_filter="744c6095a55e1f94172f15a2c18ea17b"))
+    print(primary_gpu_supports_vulkan(1,1, device_filter="NVIDIA"))
+    print(primary_gpu_supports_vulkan(1,1, device_filter="AMD"))
+
     pass
 
 
